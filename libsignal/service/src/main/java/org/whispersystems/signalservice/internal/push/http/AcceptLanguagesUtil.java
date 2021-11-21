d8d8278083023f89a748e97b5c806c7225e1ec29
devlearner <devlearner@users.noreply.github.com>
2021-11-21 16:11:36 +0800

    Fix locales with country in Accept-Language header

--- libsignal/service/src/main/java/org/whispersystems/signalservice/internal/push/http/AcceptLanguagesUtil.java
+++ libsignal/service/src/main/java/org/whispersystems/signalservice/internal/push/http/AcceptLanguagesUtil.java
@@ -8,20 +8,22 @@ import java.util.Objects;
 public class AcceptLanguagesUtil {
   public static Map<String, String> getHeadersWithAcceptLanguage(Locale locale) {
     Map<String, String> headers = new HashMap<>();
-    headers.put("Accept-Language", formatLanguages(locale.getLanguage(), Locale.US.getLanguage()));
+    headers.put("Accept-Language", formatLanguages(locale.getLanguage(), locale.getCountry(), Locale.US.getLanguage()));
 
     return headers;
   }
 
   public static String getAcceptLanguageHeader(Locale locale) {
-    return "Accept-Language:" + formatLanguages(locale.getLanguage(), Locale.US.getLanguage());
+    return "Accept-Language:" + formatLanguages(locale.getLanguage(), locale.getCountry(), Locale.US.getLanguage());
   }
 
-  private static String formatLanguages(String language, String fallback) {
+  private static String formatLanguages(String language, String country, String fallback) {
     if (Objects.equals(language, fallback)) {
       return language + ";q=1";
-    } else {
+    } else if (country.equals("")) {
       return language + ";q=1," + fallback + ";q=0.5";
+    } else {
+      return language + "-" + country + ";q=1," + language + ";q=0.9," + fallback + ";q=0.5";
     }
   }
 }
